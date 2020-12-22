1a86452e23fccc37d21a8e5a285b99d70b32bd3c
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2020-12-22 15:27:22 +0900

    solved 1 question

---
+++ Book/정렬/카드정렬하기X.py
@@ -0,0 +1,33 @@
+n = int(input())
+
+counts = []
+for i in range(n):
+    counts.append(int(input()))
+counts.sort()
+
+if n == 1:
+    print(0)
+
+else:
+    prev = counts[0]
+    total = 0
+    for i in range(1, len(counts)):
+        prev += counts[i]
+        total += prev
+    print(total)
+
+
+# import heapq
+# n = int(input())
+# counts = []
+# for i in range(n):
+#     heapq.heappush(counts, int(input()))
+#
+# total = 0
+# while len(counts) != 1:
+#     one = heapq.heappop(counts)
+#     two = heapq.heappop(counts)
+#     sum = one + two
+#     total += sum
+#     heapq.heappush(counts, sum)
+# print(total)
