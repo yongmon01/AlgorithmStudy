6e91b827cf295e89e810cf3adbac8967d3458d33
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2020-12-24 15:44:00 +0900

    solved 1 question

--- Book/Greedy/무지의먹방라이브X.py
+++ Book/Greedy/무지의먹방라이브X.py
@@ -1,3 +1,76 @@
+import heapq
+food_times, k = [3, 1, 2], 5
+
+if sum(food_times) <= k:
+    print(-1)
+
+q = []
+for i in range(len(food_times)):
+    heapq.heappush(q, (food_times[i], i+1))
+print(q)
+
+prev, curr, total, length = 0, 0, 0, len(food_times)
+while total + (q[0][0] - prev) * length <= k:
+    curr = heapq.heappop(q)[0]
+    total += (curr - prev) * length
+    length -= 1
+    prev = curr
+q.sort(key = lambda x:x[1])
+print(q[(k - total) % length][1])
+
+# 땡
+# for i in range(k):
+#     if len(food_times) == 0:
+#         print(-1)
+#     index = i % len(food_times)
+#     if food_times[index][1] == 1:
+#         del food_times[index]
+#     else:
+#         food_times[index][1] -= 1
+#     print(food_times)
+#     if i + 1 == k:
+#         print((i+1) % len(food_times)+1)
+
+
+# 땡
+# i = 0
+# while food_times:
+#     index = i % len(food_times)
+#     if food_times[index][1] == 1:
+#         del food_times[index]
+#         i -= 1
+#         k -= 1
+#     else:
+#         food_times[index][1] -= 1
+#     print(food_times)
+#     if i + 1 == k:
+#         print(food_times[i % len(food_times)][0] + 1)
+#         break
+#     i += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # 시간초과...
 
 # def solution(food_times, k):
--- Book/정렬/카드정렬하기X.py
+++ Book/정렬/카드정렬하기X.py
@@ -1,33 +1,34 @@
-n = int(input())
-
-counts = []
-for i in range(n):
-    counts.append(int(input()))
-counts.sort()
-
-if n == 1:
-    print(0)
-
-else:
-    prev = counts[0]
-    total = 0
-    for i in range(1, len(counts)):
-        prev += counts[i]
-        total += prev
-    print(total)
-
-
-# import heapq
+# 아 이거 아니네... 반례) 10 20 30 40 50
 # n = int(input())
+#
 # counts = []
 # for i in range(n):
-#     heapq.heappush(counts, int(input()))
+#     counts.append(int(input()))
+# counts.sort()
 #
-# total = 0
-# while len(counts) != 1:
-#     one = heapq.heappop(counts)
-#     two = heapq.heappop(counts)
-#     sum = one + two
-#     total += sum
-#     heapq.heappush(counts, sum)
-# print(total)
+# if n == 1:
+#     print(0)
+#
+# else:
+#     prev = counts[0]
+#     total = 0
+#     for i in range(1, len(counts)):
+#         prev += counts[i]
+#         total += prev
+#     print(total)
+
+# 이게 답..
+import heapq
+n = int(input())
+counts = []
+for i in range(n):
+    heapq.heappush(counts, int(input()))
+
+total = 0
+while len(counts) != 1:
+    one = heapq.heappop(counts)
+    two = heapq.heappop(counts)
+    sum = one + two
+    total += sum
+    heapq.heappush(counts, sum)
+print(total)
