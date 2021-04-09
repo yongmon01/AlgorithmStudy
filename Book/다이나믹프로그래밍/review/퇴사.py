85de8662c79ca27e68270d5c5ddd92da1b62a0a2
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2021-04-10 00:45:47 +0900

    reviewed 1 question

---
+++ Book/다이나믹프로그래밍/review/퇴사.py
@@ -0,0 +1,66 @@
+n = int(input())
+time = []
+point = []
+for i in range(n):
+    t, p = map(int, input().split())
+    time.append(t)
+    point.append(p)
+#
+# print(time)
+# print(point)
+
+dp = [0] * (n+1)
+for i in range(n+1):
+    if i == n:
+        continue
+    t = time[i]
+    p = point[i]
+    if i + t <= n:
+        dp[i+t] = max(dp[i+t], dp[i] + p)
+
+print(max(dp))
+
+
+# n = int(input())
+# time = []
+# point = []
+# for i in range(n):
+#     t, p = map(int, input().split())
+#     time.append(t)
+#     point.append(p)
+#
+# print(time)
+# print(point)
+#
+# def gogo(start, end, earn):
+#     # print(start, end, earn)
+#     if start > end:
+#         return earn
+#     take = gogo(start + time[start], end, earn + point[start])
+#     skip = gogo(start + 1, end, earn)
+#     if start + time[start] -1 > end:
+#         return skip
+#     return max(take, skip)
+#
+# print(gogo(0, n-1, 0))
+
+# 7
+# 3 10
+# 5 20
+# 1 10
+# 1 20
+# 2 15
+# 4 40
+# 2 200
+
+# 10
+# 1 1
+# 1 2
+# 1 3
+# 1 4
+# 1 5
+# 1 6
+# 1 7
+# 1 8
+# 1 9
+# 1 10
